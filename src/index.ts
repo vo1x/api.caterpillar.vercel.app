ffae5e00085c0e639795ee0878aa38fe2f62ef71
volx <97158976+vo1x@users.noreply.github.com>
2025-01-25 17:20:17 +0545

    fix: fix vercel prod crash

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
 
 import router from "./routes";
 import { wpProxy } from "./middleware/proxy";
@@ -20,4 +21,12 @@ app.use(
 app.route("/", router);
 app.use("/wp-json", wpProxy);
 
-export default app;
+// export default app;
+
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
